6dfbc0e4c72d928ab45587fbcee008819890591f
melissavoirol <melissa.voirol@he-arc.ch>
2015-07-01 12:03:30 +0200

    Flag countries with for iterator

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -15,6 +15,14 @@ angular.module('movieApp')
     .success(function(movie) {
     	$scope.movie  = movie;
     	$rootScope.bgImage = movie.backdrop_path;
+
+    	$scope.pays = [];
+        var countries = $scope.movie.releases.countries;
+        for (var i=0; i<countries.length; i++) {
+          if(countries[i].iso_3166_1 == 'DE' || countries[i].iso_3166_1 == 'FR' || countries[i].iso_3166_1  == 'US') {
+            $scope.pays.push(countries[i]);
+          }
+        };
     })
 
     /*search
